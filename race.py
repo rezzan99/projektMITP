77f2c999c431bcecee883a2d189c18d0affc1005
rezzan99 <ted.klatka@gmail.com>
2020-06-12 13:58:14 +0200

    added more races

--- race.py
+++ race.py
@@ -6,11 +6,31 @@ class Elf(Race):
 	DEFENSE = 6
 	STRENGTH = 4
 	AGILITY = 9
-	INTELIGENCE = 7
+	INTELIGENCE = 8
 
 class Human(Race):
 	HP = 10
 	DEFENSE = 8
-	STRENGTH = 6
-	AGILITY = 4
+	STRENGTH = 7
+	AGILITY = 5
 	INTELIGENCE = 5
+    
+class Dwarf(Race):
+    HP = 9
+    DEFENSE = 10
+    STRENGTH = 8
+    AGILITY = 4
+    INTELIGENCE = 4
+    
+
+ class Orc(Race):
+    HP = 11
+    DEFENSE = 4
+    STRENGTH = 10
+    AGILITY = 6
+    INTELIGENCE = 4
+    
+class Haflfling(Race):
+    STRENGTH = 5
+    AGILITY = 10
+    INTELIGENCE = 7
